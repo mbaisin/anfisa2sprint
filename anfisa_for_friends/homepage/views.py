c72c16e2f23d3492f256c6a79b934f6638beb275
Marat Aisin <marataysin@gmail.com>
2024-09-05 23:48:01 +0300

    homepage Q

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
+from django.db.models import Q
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+        ).filter(
+            Q(Q(is_on_main=True) & Q(is_published=True)) | Q(Q(title__contains='пломбир') & Q(is_published=True))
+        )
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
