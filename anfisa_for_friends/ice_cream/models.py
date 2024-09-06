40270cfbb91f611f1e2b888f8572b7cd0dfd537b
Marat Aisin <marataysin@gmail.com>
2024-09-06 23:41:05 +0300

    final touch

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -6,7 +6,11 @@ from core.models import PublishedModel
 class Category(PublishedModel):
     title = models.CharField(max_length=256, verbose_name="Название")
     slug = models.SlugField(max_length=64, unique=True, verbose_name="Слаг")
-    output_order = models.PositiveSmallIntegerField(default=100, verbose_name="Порядок отображения")
+    output_order = models.PositiveSmallIntegerField(
+        default=100, 
+        verbose_name="Порядок отображения"
+    )
+
 
     class Meta:
         verbose_name = "категория"
@@ -40,9 +44,15 @@ class Wrapper(PublishedModel):
     def __str__(self):
         return self.title
 
+
 class IceCream(PublishedModel):
     title = models.CharField(max_length=256, verbose_name="Название")
     description = models.TextField(verbose_name="Описание")
+    output_order = models.PositiveSmallIntegerField(
+        default=100, 
+        verbose_name="Порядок отображения"
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
@@ -63,6 +73,7 @@ class IceCream(PublishedModel):
     class Meta:
         verbose_name = "мороженое"
         verbose_name_plural = "Мороженое"
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
